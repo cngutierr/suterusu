b1076448e4fd4320f08978462555cbdaefc7274a
Christopher Gutierrez <cngutierr@gmail.com>
2016-04-13 12:52:03 -0700

    added placeholders for consumer/producer threads

--- hookts.c
+++ hookts.c
@@ -2,6 +2,17 @@
 #include <asm/uaccess.h>
 #include <linux/limits.h>
 #include <linux/utime.h>
+#include <linux/kthread.h>
+#include <linux/wait.h>
+#include <linux/circ_buf.h>
+//logger stuff
+#define LOG_FILE_STR "/tmp/.decms_log"
+volatile unsigned long to_log = 0;
+DECLARE_WAIT_QUEUE_HEAD(log_event);
+#define BBB_SIZE 2048
+char big_bad_buf[BBB_SIZE];
+struct circ_buf log_circ_buf;
+struct task_struct *logger_ts;
 
 // pointer to the system call function
 asmlinkage long (*sys_utime)(char __user *filename, struct utimbuf __user *times);
@@ -52,6 +63,8 @@ asmlinkage long n_sys_utime (char __user *filename, struct utimbuf __user *times
             DEBUG_RW("Timestamp for '%s' was changed to", (char *) debug);
             //to do perhaps add a stat call here so we can see the current timestamp
             DEBUG_RW(" a:%ld m:%ld\n",  times->actime, times->modtime);
+            to_log = 1;
+            wake_up_interruptible(&log_event);
             kfree(debug);
         }
       }
@@ -69,10 +82,39 @@ asmlinkage long n_sys_utime (char __user *filename, struct utimbuf __user *times
     return ret;
 }
 
+int consumer(void *data)
+{
+    unsigned long head, tail;
+    
+    while(1)
+    {
+        wait_event_interruptible(log_event, (to_log == 1));
+        DEBUG("Inside logger thread\n");
+        head = smp_load_acquire(&log_circ_buf.head);
+        tail = log_circ_buf.tail;
+        
+        DEBUG("Consumming data...\n");
+        //struct item *item = &log_circ_buf.buf[tail];
+        //smp_store_release(&log_circ_buf.tail,(tail + 1) & (BBB_SIZE - 1));
+        
+        to_log = 0;
+        if(kthread_should_stop() == true)
+        {
+           DEBUG("Stopping DecMS logger...\n");
+           break;
+        }
+    }
+
+    return 0;
+}
+
 void hookts_init ( void )
 {
     DEBUG("Hooking sys_utime\n");
-
+    
+    log_circ_buf.buf = big_bad_buf;
+    logger_ts = kthread_run(consumer, NULL, "decms_logger");   
+    
     // grab the function pointer from the sys_call_table
     // and start intercepting calls
     sys_utime = (void *)sys_call_table[__NR_utime];
@@ -82,6 +124,17 @@ void hookts_init ( void )
 void hookts_exit ( void )
 {
     DEBUG("Unhooking sys_utime\n");
+    
     // undo the hooking 
     hijack_stop(sys_utime);
+    
+    DEBUG("Done with hijack_stop\n");
+    
+    // stop the consumer thread
+    to_log = 1;
+    kthread_stop(logger_ts);
+    // flush the contents, if any
+    //to_log = 1;
+    //wake_up_interruptible(&log_event);
+
 }
